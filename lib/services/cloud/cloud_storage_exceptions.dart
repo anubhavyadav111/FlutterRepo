ac3a1d1b95f8940542e4ea49c2e990d5bcfc4c75
Anubhav Yadav <anubhavyadavdec10@gmial.com>
2023-07-20 17:44:51 +0530

    Step-19

---
+++ lib/services/cloud/cloud_storage_exceptions.dart
@@ -0,0 +1,21 @@
+class CloudStorageException implements Exception {
+  const CloudStorageException();
+}
+
+// C in CRUD
+
+class CloudNotCreateNoteException extends CloudStorageException {}
+
+// R in CRUD 
+
+class CouldNotGetAllNotesException extends CloudStorageException{}
+
+// U in CRUD 
+ class CouldNotUpdateNoteException extends CloudStorageException{}
+
+ //D in CRUD
+
+ class CouldNotDeleteNoteException extends CloudStorageException{}
+
+ 
+
