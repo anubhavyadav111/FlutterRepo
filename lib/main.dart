bfe8eb4c3899f07bdbd566c2e1e0d04e9f2ff008
Anubhav Yadav <anubhavyadavdec10@gmial.com>
2023-07-23 12:32:34 +0530

    Step-26

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:learningdart/views/notes/create_update_note_view.dart';
 import 'package:learningdart/views/notes/note_view.dart';
 import 'package:learningdart/views/verify_email.dart';
 
+import 'helpers/loading/loading_screen.dart';
 import 'services/auth/bloc/auth_bloc.dart';
 
 void main() {
@@ -39,21 +40,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     context.read<AuthBloc>().add(const AuthEventInitialize());
-    return BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, state) {
-        if (state is AuthStateLoggedIn) {
-          return const NotesView();
-        } else if (state is AuthStateNeedsVerification) {
-          return const VerifyEmailView();
-        } else if (state is AuthStateLoggedOut) {
-          return const LoginView();
-        } else if (state is AuthStateRegistering) {
-          return const RegisterView();
-        } else {
-          return const Scaffold(
-            body: CircularProgressIndicator(),
+  return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state.isLoading) {
+          LoadingScreen().show(
+            context: context,
+            text: state.loadingText ?? 'Please wait a moment',
           );
+        } else {
+          LoadingScreen().hide();
         }
+      },
+      builder: (context, state) {
+        return BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthStateLoggedIn) {
+              return const NotesView();
+            } else if (state is AuthStateNeedsVerification) {
+              return const VerifyEmailView();
+            } else if (state is AuthStateLoggedOut) {
+              return const LoginView();
+            } else if (state is AuthStateRegistering) {
+              return const RegisterView();
+            } else {
+              return const Scaffold(
+                body: CircularProgressIndicator(),
+              );
+            }
+          },
+        );
       },
     );
   }
